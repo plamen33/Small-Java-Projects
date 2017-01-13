8825b8c4a19e98fe1e8a8091c054098485e92666
Plamen <plam_en27@abv.bg>
2017-01-13 22:11:20 +0200

    Calculator JavaFX Project 1/ added fix when dividing to equal instead of number try catch structure 2/ Project tested 3/ finished Calculator JavaFX Project

--- CalculatorJavaFX/src/calculator/Controller.java
+++ CalculatorJavaFX/src/calculator/Controller.java
@@ -1,67 +1,71 @@
 package calculator;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.text.Text;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+        import javafx.scene.text.Text;
+        import javafx.scene.control.TextField;
+        import javafx.stage.Stage;
 
 public class Controller {
 
-    
     @FXML
     private Text output;
 
     private double number1 = 0; // by default number1 value equals 0
     private String operator = "";
     private boolean start = true;
-	private String clearCommand = "";
+    private String clearCommand = "";
 
     private Model model = new Model();
-
     @FXML
-    private void processNumpad(ActionEvent event) {
-        if (start) {
+    private void processNumpad(ActionEvent event){
+        if(start){
             output.setText("");
             start = false;
         }
-
         String value = ((Button)event.getSource()).getText();  // returns the text of the Button which is on the button
         output.setText(output.getText() + value);  // get the value - we append the value at the end
     }
-
     @FXML
-    private void processOperator(ActionEvent event) {
+    private void processOperator(ActionEvent event){
         String value = ((Button)event.getSource()).getText();
+        try {
+            if (!"=".equals(value) && !"Clear".equals(value)) {  // we check if value does not equal the final equal sign as it is the final calculate sign
+                if (!operator.isEmpty()) {
+                    return;
+                }
 
-         if (!"=".equals(value) && !"Clear".equals(value)) {  // we check if value does not equal the final equal sign as it is the final calculate sign
-            if (!operator.isEmpty()){
-                return;
+                operator = value;
+                number1 = Double.parseDouble(output.getText());
+                output.setText("");
             }
-            operator = value;
-            number1 = Double.parseDouble(output.getText());
-            output.setText("");
+            else if (value.equals("Clear")) {
+                number1 = 0;
+                operator = ""; // clear the operator
+                start = true;
+                output.setText("");
+            }
+            else {
+                if (operator.isEmpty()) {
+                    return;
+                }
+                double number2 = Double.parseDouble(output.getText());
+                output.setText(String.valueOf(model.calculate(number1, number2, operator)));
+                double result = model.calculate(number1, number2, operator);
+                number1 = result; // in case one decides to do actions further
+                operator = ""; // clear the operator
+                start = true;
+            }
         }
-		else if (value.equals("Clear")) {
+        catch(Exception e){
             number1 = 0;
             operator = ""; // clear the operator
             start = true;
-            output.setText("");
-        }
-        else {
-            if (operator.isEmpty()){
-                return;
-            }
-            double number2 = Double.parseDouble(output.getText());
-            output.setText(String.valueOf(model.calculate(number1, number2, operator)));
-            double result = model.calculate(number1, number2, operator);
-            number1 = result; // in case one decides to do actions further
-            operator = ""; // clear the operator
-            start = true;
+            output.setText("Wrong actions");
         }
     }
-	//This is done to change the Title of the program window
+    //This is done to change the Title of the program window
     @FXML
     private TextField input;
     @FXML
@@ -69,4 +73,4 @@ public class Controller {
         Stage primStage = (Stage)input.getScene().getWindow();
         primStage.setTitle(input.getText());
     }
- }
+}
